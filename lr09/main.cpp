8ac7dae13168ae27192e0d954379809bf0c5b54f
KeltorHD <keltorplaylife@gmail.com>
2020-11-02 20:37:39 +0300

    add pdf

--- lr09/main.cpp
+++ lr09/main.cpp
@@ -11,7 +11,7 @@ int main()
 	system("chcp 65001>nul");
 
 	std::cout << "Максимальное количество элементов для Queue<double>:\t " << max_size_struct(Queue<double>()) << std::endl;
-	std::cout << "Максимальное количество элементов для Queue<dloat>:\t " << max_size_struct(Queue<float>()) << std::endl;
+	std::cout << "Максимальное количество элементов для Queue<float>:\t " << max_size_struct(Queue<float>()) << std::endl;
 	std::cout << "Максимальное количество элементов для Queue<int>:\t " << max_size_struct(Queue<int>()) << std::endl;
 	std::cout << "Максимальное количество элементов для Queue<char>:\t " << max_size_struct(Queue<char>()) << std::endl;
 
@@ -33,5 +33,5 @@ uint64_t max_size_struct(const Struct&)
 
 	memory_free -= st_size; /*вычитаем количество байт на структуру без данных*/
 
-	return memory_free / Struct::bite_per_element(); /*делим доступное кол-во байт на кол-во байт на один элемент*/
+	return (memory_free / Struct::bite_per_element()); /*делим доступное кол-во байт на кол-во байт на один элемент*/
 }
