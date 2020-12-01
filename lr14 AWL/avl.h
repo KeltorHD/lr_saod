353fe3c7b03fe645377de1e35d5308e4e5fe7d61
KeltorHD <keltorplaylife@gmail.com>
2020-12-01 22:34:11 +0300

    lr14 done

--- lr14 AWL/avl.h
+++ lr14 AWL/avl.h
@@ -1,50 +1,60 @@
 #pragma once
 
+#include <iostream>
 #include <cstdint>
 
 
-template<typename T>
+template<typename value_t>
 class AVL
 {
 public:
 	AVL() : top(nullptr) {}
 	~AVL() { this->clear(this->top); };
 
-	void add(const T& value);
+	void add(const value_t& value);
+	void remove(const value_t& value);
+
+	void print(std::ostream& os) const;
 
 private:
 	struct node
 	{
-		T value;
+		value_t value;
 		uint8_t height;
 		node* left;
 		node* right;
 
-		node(const T& value) :left(nullptr), right(nullptr), value(value), height(1) {}
-
-		uint8_t get_height() { this->height ? this->height : 0; }
-		uint8_t bal_factor() { this->left->get_height() - this->right->get_height(); }
-		void fix_height() 
-		{
-			uint8_t hl = this->left->get_height();
-			uint8_t hr = this->right->get_height();
-			this->height = (hl > hr ? hl : hr) + 1;
-		}
+		node(const value_t& value) :left(nullptr), right(nullptr), value(value), height(1) {}
 	};
 
 	node* top;
 
+	/*работа с node*/
+	uint8_t get_height(node* p) { return p ? p->height : 0; }
+	uint8_t bal_factor(node* p) { return get_height(p->right) - get_height(p->left); }
+	void fix_height(node* p)
+	{
+		uint8_t hl = get_height(p->left);
+		uint8_t hr = get_height(p->right);
+		p->height = (hl > hr ? hl : hr) + 1;
+	}
+
+	/*операции над node*/
 	node* rotate_right(node* to_rotate);
 	node* rotate_left(node* to_rotate);
 	node* balance(node* to_balance);
+	node* findmin(node* p);
+	node* removemin(node* p);
 
-	void add_r(node*& to_add, const T& value);
+	node* add_r(node* to_add, const value_t& value);
+	node* remove_r(node* to_remove, const value_t& value);
+	void print_r(std::ostream& os, int level, node* to_print) const;
 
 	void clear(node* to_delete);
 };
 
-template<typename T>
-inline void AVL<T>::add(const T& value)
+template<typename value_t>
+inline void AVL<value_t>::add(const value_t& value)
 {
 	if (!this->top)
 	{
@@ -54,78 +64,171 @@ inline void AVL<T>::add(const T& value)
 
 	if (value < this->top->value)
 	{
-		this->add_r(this->top->left, value);
+		this->top->left = this->add_r(this->top->left, value);
 	}
 	else
 	{
-		this->add_r(this->top->right, value);
+		this->top->right = this->add_r(this->top->right, value);
 	}
-	this->balance(this->top);
+	this->top = this->balance(this->top);
 }
 
-template<typename T>
-inline AVL<T>::node* AVL<T>::rotate_right(node* to_rotate)
+template<typename value_t>
+inline void AVL<value_t>::remove(const value_t& value)
+{
+	if (this->top)
+	{
+		if (value < this->top->value)
+			this->top->left = remove_r(this->top->left, value);
+		else if (value > this->top->value)
+			this->top->right = remove_r(this->top->right, value);
+		else //  value == top->value
+		{
+			node* q = this->top->left;
+			node* r = this->top->right;
+			delete this->top;
+			if (!r) 
+			{
+				this->top = q;
+				return;
+			}
+			node* min = findmin(r);
+			min->right = removemin(r);
+			min->left = q;
+			this->top = balance(min);
+		}
+		this->top = this->balance(this->top);
+	}
+}
+
+template<typename value_t>
+inline void AVL<value_t>::print(std::ostream& os) const
+{
+	int level{ 0 };
+	this->print_r(os, level, this->top);
+}
+
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::rotate_right(node* to_rotate)
 {
 	node* q = to_rotate->left;
 	to_rotate->left = q->right;
 	q->right = to_rotate;
-	to_rotate->fix_height();
-	to_rotate->fix_height();
+	fix_height(to_rotate);
+	fix_height(q);
 	return q;
 }
 
-template<typename T>
-inline AVL<T>::node* AVL<T>::rotate_left(node* to_rotate)
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::rotate_left(node* to_rotate)
 {
 	node* p = to_rotate->right;
 	to_rotate->right = p->left;
 	p->left = to_rotate;
-	to_rotate->fix_height();
-	p->fix_height();
+	fix_height(to_rotate);
+	fix_height(p);
 	return p;
 }
 
-template<typename T>
-inline AVL<T>::node* AVL<T>::balance(node* to_balance)
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::balance(node* to_balance)
 {
-	to_balance->fix_height();
-	if (to_balance->bal_factor() == 2)
+	fix_height(to_balance);
+	if (bal_factor(to_balance) == 2)
 	{
-		if (to_balance->right->bal_factor() < 0)
+		if (bal_factor(to_balance->right) < 0)
 			to_balance->right = this->rotate_right(to_balance->right);
 		return this->rotate_left(to_balance);
 	}
-	if (to_balance->bal_factor() == -2)
+	if (bal_factor(to_balance) == -2)
 	{
-		if (to_balance->left->bal_factor() > 0)
+		if (bal_factor(to_balance->left) > 0)
 			to_balance->left = this->rotate_left(to_balance->left);
 		return this->rotate_right(to_balance);
 	}
 	return to_balance; // балансировка не нужна
 }
 
-template<typename T>
-inline void AVL<T>::add_r(node*& to_add, const T& value)
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::findmin(node* p)
+{
+	return p->left ? findmin(p->left) : p;
+}
+
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::removemin(node* p)
+{
+	if (p->left == 0)
+		return p->right;
+	p->left = removemin(p->left);
+	return balance(p);
+}
+
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::add_r(node* to_add, const value_t& value)
 {
 	if (!to_add)
 	{
 		to_add = new node(value);
-		return;
+		return to_add;
 	}
 
 	if (value < to_add->value)
 	{
-		this->add_r(to_add->left, value);
+		to_add->left = this->add_r(to_add->left, value);
 	}
 	else
 	{
-		this->add_r(to_add->right, value);
+		to_add->right = this->add_r(to_add->right, value);
 	}
-	balance(to_add);
+	return balance(to_add);
 }
 
-template<typename T>
-inline void AVL<T>::clear(node* to_delete)
+template<typename value_t>
+inline typename AVL<value_t>::node* AVL<value_t>::remove_r(node* to_remove, const value_t& value)
+{
+	if (value < to_remove->value)
+		to_remove->left = remove_r(to_remove->left, value);
+	else if (value > to_remove->value)
+		to_remove->right = remove_r(to_remove->right, value);
+	else //  value == to_remove->value
+	{
+		node* q = to_remove->left;
+		node* r = to_remove->right;
+		delete to_remove;
+		if (!r)
+		{
+			return q;
+		}
+		node* min = findmin(r);
+		min->right = removemin(r);
+		min->left = q;
+		return balance(min);
+	}
+	return this->balance(to_remove);
+}
+
+template<typename value_t>
+inline void AVL<value_t>::print_r(std::ostream& os, int level, node* to_print) const
+{
+	if (!to_print)
+		return;
+
+	int l = level;
+	for (size_t i = 0; i < l; i++)
+	{
+		os << "-";
+	}
+	os << to_print->value << std::endl;
+
+	l++;
+
+	this->print_r(os, l, to_print->left);
+	this->print_r(os, l, to_print->right);
+}
+
+template<typename value_t>
+inline void AVL<value_t>::clear(node* to_delete)
 {
 	if (to_delete)
 	{
--- lr14 AWL/main.cpp
+++ lr14 AWL/main.cpp
@@ -1,11 +1,61 @@
 #include <iostream>
+#include <fstream>
 #include "avl.h"
+
+struct data
+{
+	int id;
+	char field;
+
+};
+
+std::ostream& operator<<(std::ostream& os, const data& d)
+{
+	os << d.id << " " << d.field;
+	return os;
+}
+
+bool operator<(const data& d1, const data& d2)
+{
+	return d1.id < d2.id;
+}
+
+bool operator>(const data& d1, const data& d2)
+{
+	return d1.id > d2.id;
+}
 
 int main()
 {
 	system("chcp 65001>nul");
 
-	AVL<int> a;
+	AVL<data> tree;
+
+	std::ifstream ifs("data.txt");
+
+	int id{};
+	char field{};
+	size_t length{};
+	while (ifs >> id >> field)
+	{
+		tree.add({ id, field });
+		length++;
+	}
+	ifs.close();
+
+	tree.print(std::cout);
+	std::cout << std::endl;
+
+	tree.remove({ 190,'f' });
+	tree.print(std::cout);
+	std::cout << std::endl;
+
+	/*for (size_t i = 0; i < length; i++)
+	{
+		tree.remove({ int(i),'f' });
+		tree.print(std::cout);
+		std::cout << std::endl;
+	}*/
 
 	return bool(std::cout);
 }
